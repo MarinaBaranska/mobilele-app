38910b76a85be04f503062fdac92d8017221500a
MarinaBaranska <marina.baranska@gmail.com>
2024-04-22 08:22:26 +0300

    My commit

---
+++ mobilele/src/main/java/org/softuni/mobilele/web/UserLoginController.java
@@ -0,0 +1,20 @@
+package org.softuni.mobilele.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//клас който посреща htpp заявките и чрез него ги обработваме
+@Controller
+public class UserLoginController {
+
+    //оказваме какви методи искаме да обработваме - get push..
+
+    //метод обработващ get заявки
+    @GetMapping("/user/login")
+    public String login(){
+        return "auth-login"; //взимаме името от templates html
+
+    }
+
+}
